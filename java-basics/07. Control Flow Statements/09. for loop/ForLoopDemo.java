bddfd1ea1a2c307bed406704d03f464b808a4a31
Dimitri Bolotnikov <148213666+DitoGeorgia@users.noreply.github.com>
2025-04-29 19:23:13 +0400

    Delete java-basics directory

--- java-basics/07. Control Flow Statements/09. for loop/ForLoopDemo.java
+++
@@ -1,9 +0,0 @@
-public class ForLoopDemo {
-    public static void main(String[] args) {
-
-        // A basic for loop that runs 5 times
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i + ": Hello World");
-        }
-    }
-}
