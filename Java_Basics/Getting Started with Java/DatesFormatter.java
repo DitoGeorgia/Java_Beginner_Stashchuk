d08c9b00c668c211b231fbfdeb6021d94dfaaadc
DitoGeorgia <ditogeorgia@gmail.com>
2025-10-14 20:43:44 +0400

    add

--- Java_Basics/Getting Started with Java/DatesFormatter.java
+++ Java_Basics/Getting Started with Java/DatesFormatter.java
@@ -1,5 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DatesFormatter {
     public static void main(String[] args) {
-
+        Date currentDate = new Date();
+        SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEEE", Locale.ENGLISH);
+        String dayOfWeek = dayOfWeekFormat.format(currentDate);
+        System.out.println("Current Day of the Week: " + dayOfWeek);
     }
 }
